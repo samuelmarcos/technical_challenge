a3322389878d17d3a893c33e9e774bb9d0032c7f
samuelmarcos <samuelmarcos9580@gmail.com>
2023-09-20 18:26:49 -0300

    chore: ensure DbAddAccount calls hasher with correct values

--- src/data/usecases/account/add-account/db-add-account.ts
+++ src/data/usecases/account/add-account/db-add-account.ts
@@ -1,14 +1,18 @@
+import { Hasher } from '@/data/protocols/cryptography/hasher'
 import { AccountModel, LoadAccountByEmailRepository, AddAccountParams, AddAccount} from './db-add-account-protocols'
 
 export class DbAddAccount implements AddAccount {
 
-  constructor(private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository) {}
+  constructor(private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
+              private readonly hasher: Hasher) {}
 
   public async add(accountData: AddAccountParams): Promise<AccountModel> {
 
     const account = await this.loadAccountByEmailRepository.loadByEmail(accountData.email)
 
     if(account) return null
+
+    await this.hasher.hash(accountData.password)
 
     return Promise.resolve({
       id: 'any_id',
