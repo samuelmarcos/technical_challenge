995a9e291c30a1dc281bcf846715de17d95faff2
samuelmarcos <samuelmarcos9580@gmail.com>
2023-09-20 18:10:48 -0300

    test: ensure DbAddAccount calls LoadAccountByEmailRepository with correct values

---
+++ src/data/usecases/account/add-account/db-add-account.spec.ts
@@ -0,0 +1,27 @@
+import { DbAddAccount } from './db-add-account'
+import { mockLoadAccountByEmailRepository } from '@/tests/mocks/data/db'
+import { AddAccount } from './db-add-account-protocols'
+import { LoadAccountByEmailRepository } from '@/data/protocols/db/account/load-account-by-email-repository'
+import { mockAccountModel, mockAddAccountParams } from '@/tests/mocks/domain/mock-account'
+
+
+type SutTypes = {
+  sut: AddAccount
+  loadAccountByEmailRepositoryStub: LoadAccountByEmailRepository
+}
+
+const makeSut = ():SutTypes => {
+
+  const loadAccountByEmailRepositoryStub = mockLoadAccountByEmailRepository()
+  const sut = new DbAddAccount(loadAccountByEmailRepositoryStub)
+  return { sut, loadAccountByEmailRepositoryStub }
+}
+
+describe('DbAddAccount tests', () => {
+  test('should return null if LoadAccountByEmailRepository not returns null', async () => {
+    const { sut, loadAccountByEmailRepositoryStub } = makeSut()
+    jest.spyOn(loadAccountByEmailRepositoryStub,'loadByEmail').mockReturnValueOnce(new Promise((resolve)=>{resolve(mockAccountModel())}))
+    const account = await sut.add(mockAddAccountParams())
+    expect(account).toBe(null)
+  })
+})
