14cc5ef8de47b70d109b200bb8f4529c72de0e51
samuelmarcos <samuelmarcos9580@gmail.com>
2023-09-20 14:37:09 -0300

    test: ensure SignupController return 500 if AddAccount usecase throws

--- tests/presentation/signup-controller.spec.ts
+++ tests/presentation/signup-controller.spec.ts
@@ -2,22 +2,45 @@ import { SignupController } from '@/presentation/controller/login/signup/signup-
 import { mockValidation } from '@/tests/mocks/validation/index'
 import { Controller, Validation } from '@/presentation/protocols'
 import { mockSignupHttpRequest } from '@/tests/mocks/http'
-import { MissingParamError } from '@/presentation/errors'
-import { badRequest } from '@/presentation/helpers/http-helpers'
+import { MissingParamError, ServerError } from '@/presentation/errors'
+import { badRequest, serverError } from '@/presentation/helpers/http-helpers'
+import { mockAddAccount } from '@/tests/mocks/presentation'
+import { AddAccount } from '../mocks/domain/usecases/add-account'
 
 type SutTypes = {
   sut: Controller
   validationStub: Validation
+  addAccountStub: AddAccount
 }
 
 const makeSut = (): SutTypes => {
   const validationStub = mockValidation()
-  const sut = new SignupController(validationStub)
+  const addAccountStub = mockAddAccount()
+  const sut = new SignupController(validationStub, addAccountStub)
 
-  return { sut, validationStub }
+  return { sut, validationStub, addAccountStub }
 }
 
 describe('SignupController tests', () => {
+  test('should call AddAccount with correct email', async () => {
+    const { sut, addAccountStub } = makeSut()
+    const addSpy = jest.spyOn(addAccountStub, 'add')
+
+    await sut.handle(mockSignupHttpRequest())
+    expect(addSpy).toHaveBeenCalledWith({
+        name: 'any_name',
+        email: 'any_email@email.com',
+        password: 'any_password',
+    })
+  })
+
+  test('should return 500 if AddAccount throws', async () => {
+    const { sut, addAccountStub } = makeSut()
+    jest.spyOn(addAccountStub, 'add').mockImplementationOnce(() => { return new Promise((resolve, reject) => reject(new Error()))})
+    const httpResponse = await sut.handle(mockSignupHttpRequest())
+    expect(httpResponse).toEqual(serverError(new ServerError('any_stack')))
+  })
+
   test('shoud call validation correct email', async () => {
     const { sut, validationStub } = makeSut()
     const validationSpied = jest.spyOn(validationStub, 'validate')
