724854d5ef3ab90f569fcabbe7f7c84160f5dfc0
samuelmarcos <samuelmarcos9580@gmail.com>
2023-09-21 15:14:24 -0300

    feat: ensure email validation calls EmailValidator on succes and fail case

---
+++ src/validation/validators/email-validation.ts
@@ -0,0 +1,12 @@
+import { InvalidParamError } from '@/presentation/errors'
+import { EmailValidator } from '../protocols/email-validator'
+import { Validation } from '@/presentation/protocols'
+
+export class EmailValidation implements Validation {
+    constructor(private readonly fieldName: string, private readonly emailValidator: EmailValidator) {}
+
+    validate(input: any): any {
+        const isValid = this.emailValidator.isValid(input[this.fieldName])
+        if(!isValid) return new InvalidParamError(this.fieldName)
+    }
+}
