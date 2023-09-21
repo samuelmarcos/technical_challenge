14176ec84975f58852e6baa116b75b3c0cc83858
samuelmarcos <samuelmarcos9580@gmail.com>
2023-09-21 15:14:55 -0300

    test: ensure email validation calls EmailValidator on succes and fail case

--- tests/mocks/validation/mock-email-validator.ts
+++ tests/mocks/validation/mock-email-validator.ts
@@ -1,10 +1,10 @@
-import { EmailValidator } from "@/validation/protocols/email-validator"
+import { EmailValidator } from '@/validation/protocols/email-validator';
 
 export const mockEmailValidator = (): EmailValidator => {
   class EmailValidatorStub implements EmailValidator {
-      isValid(email: string): boolean {
-          return true
-      }
+    isValid(email: string): boolean {
+      return true;
+    }
   }
-  return new EmailValidatorStub()
-}
+  return new EmailValidatorStub();
+};
