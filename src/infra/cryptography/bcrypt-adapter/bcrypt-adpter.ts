f4cddfdb5aa255d5871709c0dd13de8544b42e3c
samuelmarcos <samuelmarcos9580@gmail.com>
2023-09-21 10:06:47 -0300

    feat: call hash with correct values and return a valid hash

---
+++ src/infra/cryptography/bcrypt-adapter/bcrypt-adpter.ts
@@ -0,0 +1,13 @@
+import { Hasher } from '@/data/protocols/cryptography/hasher'
+import bcrypt from 'bcrypt'
+
+export class BcryptAdapter implements Hasher {
+    private readonly salt: number
+    constructor(salt: number) {
+        this.salt = salt
+    }
+    async hash(value: string): Promise<string> {
+        const hash =  await bcrypt.hash(value, this.salt)
+        return hash
+    }
+}
