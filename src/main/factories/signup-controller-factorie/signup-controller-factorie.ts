34e1a8d437c9976c3289ef950b737101326d7c2a
samuelmarcos <samuelmarcos9580@gmail.com>
2023-09-21 18:04:12 -0300

    refactor: SignupController

--- src/main/factories/signup-controller-factorie/signup-controller-factorie.ts
+++ src/main/factories/signup-controller-factorie/signup-controller-factorie.ts
@@ -1,18 +1,23 @@
+import { LoadConfig } from '@/configuration/load-environment'
 import { DynamoHelper } from '@/infra/db/dynamo/helpers/dynamo-helper'
 import { AccountDynamoRepository } from '@/infra/db/dynamo/account/account-dynamo-repository'
-import { BcryptAdapter } from '@/infra/cryptography/bcrypt-adapter/bcrypt-adpter'
 import { DbAddAccount } from '@/data/usecases/account/add-account/db-add-account'
 import { makeSignupValidation } from '@/main/factories/signup-controller-factorie/validation-factorie/signup-validation-facotire'
-import {} from '@/data/usecases/account/add-account/'
+import { DbAuthentication } from '@/data/usecases/account/authentication/db.authentication'
 import { SignupController } from '@/presentation/controller/login/signup/signup-controller'
+import {BcryptAdapter } from '@/infra/cryptography/bcrypt-adapter/bcrypt-adpter'
+import { JwtAdapter } from '@/infra/cryptography/jwt-adpter/jwt-adapter'
 import { Controller } from '@/presentation/protocols'
 
 export const makeSignupController = (): Controller => {
+  const config = LoadConfig.getInstance().getInveronments()
   const client = DynamoHelper.getClient()
   const repository = new AccountDynamoRepository(client)
   const bcryptAdapter = new BcryptAdapter(8)
+  const jwtAdapter = new JwtAdapter(config.SECRET)
   const addAccount = new DbAddAccount(repository, bcryptAdapter, repository)
+  const authentication = new DbAuthentication(repository, bcryptAdapter, jwtAdapter, repository)
   const validationComposite = makeSignupValidation()
 
-  return new SignupController(validationComposite, addAccount)
+  return new SignupController(validationComposite, addAccount, authentication)
 }
--- src/main/lambda/login/signup/signup-lambda.ts
+++ src/main/lambda/login/signup/signup-lambda.ts
@@ -1,9 +1,10 @@
+import 'module-alias'
 import { APIGatewayProxyEventV2, APIGatewayProxyResultV2  } from 'aws-lambda'
 import { makeSignupController } from '@/main/factories/signup-controller-factorie/signup-controller-factorie'
 import { Controller } from '@/presentation/protocols'
 import { EventMapper } from '@/main/helpers/event-mapper'
 
-export const signupFunction = async (event: APIGatewayProxyEventV2) => {
+export const signupFunction = async (event: APIGatewayProxyEventV2): Promise<APIGatewayProxyResultV2> => {
   const httpRequest = EventMapper.mapEvent(event)
   const controller: Controller = makeSignupController()
 
