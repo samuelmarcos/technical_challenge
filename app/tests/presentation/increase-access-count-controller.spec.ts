1faf7e0e02fb1a604408e6dfbd399f55e00ef99d
samuelmarcos <samuelmarcos9580@gmail.com>
2023-09-25 16:14:04 -0300

    test: ensure IncreaseAccessCountController throws if IncreaseCount throws

--- app/tests/presentation/increase-access-count-controller.spec.ts
+++ app/tests/presentation/increase-access-count-controller.spec.ts
@@ -1,6 +1,8 @@
 import { IncreaseAccessCountController } from '@/presentation/controller/count/increase/increase-access-count-controller'
 import { IncreaseCount , Controller, HttpRequest } from '@/presentation/controller/count/increase/increase-access-count-controller-protocols'
 import { mockIncreaseCount } from '@/tests/mocks/presentation/mock-increase-count'
+import { ServerError } from '@/presentation/errors'
+import { serverError } from '@/presentation/helpers/http-helpers'
 
 
 type SutTypes = {
@@ -26,5 +28,12 @@ describe('IncreaseAccessCountController tests', () => {
 
     await sut.handle(mockRequest)
     expect(increaseSpied).toHaveBeenCalled()
+  })
+
+  test('should return 500 if IncreaseCount throws', async () => {
+    const { sut, increaseCount } = makeSut()
+    jest.spyOn(increaseCount, 'increase').mockImplementationOnce(() => { return new Promise((resolve, reject) => reject(new Error()))})
+    const httpResponse = await sut.handle(mockRequest)
+    expect(httpResponse).toEqual(serverError(new ServerError('any_stack')))
   })
 })
