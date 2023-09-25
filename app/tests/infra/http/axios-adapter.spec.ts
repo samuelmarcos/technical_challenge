14c0528efac0c811578902fe6d79ab86558888d6
samuelmarcos <samuelmarcos9580@gmail.com>
2023-09-25 14:26:30 -0300

    test: ensure AxiosAdapter throws if axios instance throws

--- app/tests/infra/http/axios-adapter.spec.ts
+++ app/tests/infra/http/axios-adapter.spec.ts
@@ -1,6 +1,7 @@
 import { AxiosAdapter } from '@/infra/http/axios-adapter'
 import { HttpRequester } from '@/service/count/protocols'
 import { mockAxiosInstance } from '@/tests/mocks/helpers/mock-axios'
+import { throwError } from '@/tests/mocks/helpers/test-helper'
 import { AxiosInstance } from 'axios'
 
 type SutTypes = {
@@ -21,5 +22,12 @@ describe('AxiosAdapter tests', () => {
     const axiosInstanceSpied = jest.spyOn(axiosInstanceStub, 'get')
     await sut.get('any_url')
     expect(axiosInstanceSpied).toHaveBeenCalled()
+  })
+
+  test('should return 500 if axios instance throws', async () => {
+    const { sut, axiosInstanceStub } = makeSut()
+    jest.spyOn(axiosInstanceStub, 'get').mockImplementationOnce(throwError)
+    const promiseAccount =  sut.get('any_url')
+    await expect(promiseAccount).rejects.toThrow()
   })
 })
