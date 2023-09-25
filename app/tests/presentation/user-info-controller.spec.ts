85448d58922874fd288479ccc3822dfd5831de15
samuelmarcos <samuelmarcos9580@gmail.com>
2023-09-25 15:08:51 -0300

    test: ensure UserInfoController calls validator with correct email

--- app/tests/presentation/user-info-controller.spec.ts
+++ app/tests/presentation/user-info-controller.spec.ts
@@ -15,7 +15,7 @@ type SutTypes = {
 const makeSut = (): SutTypes => {
   const validationStub = mockValidation()
   const getUserInfoStub = mockgetUserInfo()
-  const sut = new UserInfoController(getUserInfoStub)
+  const sut = new UserInfoController(getUserInfoStub, validationStub)
 
   return { sut, validationStub, getUserInfoStub }
 }
@@ -34,5 +34,12 @@ describe('UserInfoController tests', () => {
     jest.spyOn(getUserInfoStub, 'getInfo').mockImplementationOnce(() => { return new Promise((resolve, reject) => reject(new Error()))})
     const httpResponse = await sut.handle(mockGetInfoHttpRequest())
     expect(httpResponse).toEqual(serverError(new ServerError('any_stack')))
+  })
+
+  test('shoud call validation correct email', async () => {
+    const { sut, validationStub } = makeSut()
+    const validationSpied = jest.spyOn(validationStub, 'validate')
+    await sut.handle(mockGetInfoHttpRequest())
+    expect(validationSpied).toHaveBeenCalledWith(mockGetInfoHttpRequest().params)
   })
 })
