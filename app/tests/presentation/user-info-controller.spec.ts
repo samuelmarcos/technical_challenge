2195acf7aa7be8302f5cf4ba23f303ee5883ef8f
samuelmarcos <samuelmarcos9580@gmail.com>
2023-09-25 15:05:50 -0300

    test: ensure UserInfoController throws if GetUserInfo throws

--- app/tests/presentation/user-info-controller.spec.ts
+++ app/tests/presentation/user-info-controller.spec.ts
@@ -3,8 +3,8 @@ import { UserInfoController } from '@/presentation/controller/info/user/user-inf
 import { mockValidation } from '@/tests/mocks/validation'
 import { mockgetUserInfo } from '../mocks/presentation'
 import { mockGetInfoHttpRequest } from '../mocks/http'
-import { serverError } from './helpers/http-helpers'
-import { ServerError } from './errors'
+import {  serverError } from '@/presentation/helpers/http-helpers'
+import {  ServerError } from '@/presentation/errors'
 
 type SutTypes = {
   sut: Controller
@@ -29,5 +29,10 @@ describe('UserInfoController tests', () => {
     expect(addSpy).toHaveBeenCalledWith('any_email@email.com')
   })
 
-  
+  test('should return 500 if AddAccount throws', async () => {
+    const { sut, getUserInfoStub } = makeSut()
+    jest.spyOn(getUserInfoStub, 'getInfo').mockImplementationOnce(() => { return new Promise((resolve, reject) => reject(new Error()))})
+    const httpResponse = await sut.handle(mockGetInfoHttpRequest())
+    expect(httpResponse).toEqual(serverError(new ServerError('any_stack')))
+  })
 })
