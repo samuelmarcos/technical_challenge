bbb9147a86a526ea2624d15ab4d62c4d3ead4f9e
samuelmarcos <samuelmarcos9580@gmail.com>
2023-09-25 15:08:39 -0300

    feat: ensure UserInfoController calls validator with correct email

--- app/src/presentation/controller/info/user/user-info-controller.ts
+++ app/src/presentation/controller/info/user/user-info-controller.ts
@@ -1,12 +1,16 @@
 import { serverError } from '@/presentation/helpers/http-helpers'
-import { Controller, HttpRequest, HttpResponse, GetUserInfo } from './user-info-controller-protocols'
+import { Controller, HttpRequest, HttpResponse, GetUserInfo, Validation } from './user-info-controller-protocols'
 
 export class UserInfoController implements Controller {
 
-  constructor(private readonly getUserInfo: GetUserInfo) {}
+  constructor(private readonly getUserInfo: GetUserInfo,
+              private readonly validator: Validation) {}
   public async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
 
     try {
+
+      await this.validator.validate(httpRequest.params)
+      
       await this.getUserInfo.getInfo(httpRequest.params.email)
     
       return Promise.resolve({
