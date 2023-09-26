0218dbf42feac36a936035c0bcc30e5f5a4df3ae
samuelmarcos <samuelmarcos9580@gmail.com>
2023-09-25 21:19:19 -0300

    refactor: eslint parse file

--- app/src/presentation/controller/count/access/add-count-controller.ts
+++ app/src/presentation/controller/count/access/add-count-controller.ts
@@ -1,18 +1,21 @@
-import { ok, serverError } from '@/presentation/helpers/http-helpers'
-import { AddCount, Controller, HttpRequest, HttpResponse } from './add-count-controller-protocols'
+import { ok, serverError } from '@/presentation/helpers/http-helpers';
+import {
+  AddCount,
+  Controller,
+  HttpRequest,
+  HttpResponse,
+} from './add-count-controller-protocols';
 
 export class AddCountController implements Controller {
-
   constructor(private readonly addCount: AddCount) {}
 
   public async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-
     try {
-      const accessNumber = await this.addCount.count()
+      const accessNumber = await this.addCount.count();
 
-      return ok(accessNumber)
-    } catch(error: any) {
-      return serverError(error)
+      return ok(accessNumber);
+    } catch (err: any) {
+      return serverError(err);
     }
   }
 }
